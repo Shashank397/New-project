7cc7a9468d12970a2044adcfe89b99f637b8a362
shashank <shashanksoni397@gmail.com>
2019-08-30 15:35:17 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 public class Hello
 {
-public static void main(String args[])
+public static void main(String args[]) throws InterruptedException
     {
         Thread.sleep(1000);
         System.out.println("Hello java");
